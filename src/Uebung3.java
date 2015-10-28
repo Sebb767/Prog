fc71e2cbd67f1d2ccde0bf60b55f36efc67be20f
Sebastian Kaim <sebb@sebb767.de>
2015-10-28 14:23:07 +0100

    Aufgabe 1

--- src/Uebung3.java
+++ src/Uebung3.java
@@ -7,35 +7,48 @@ public class Uebung3 {
 
     public static void main()
     {
-
+        Sieben();
         zeichenanalyse();
+        Wuerfel();
+    }
+
+    // Aufgabe 1
+    public static void Sieben()
+    {
+        try {
+            System.out.println("Die Eingabe ist "+(Integer.parseInt((new BufferedReader(new InputStreamReader(System.in))).readLine())%7==0?"":"nicht ")+"durch 7 teilbar.");
+        }
+        catch (Exception e) {} // idc
     }
 
 
     // Aufgabe 3
     public static void zeichenanalyse()
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char eingabe = br.read();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            char eingabe = (char)br.read();
 
-        String ausgabe = "\n";
+            String ausgabe = "\n";
 
-        if("ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(eingabe) != -1)
-           ausgabe += "Großbuchstabe\n";
+            if("ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(eingabe) != -1)
+               ausgabe += "Großbuchstabe\n";
 
-        if("0123456789abcdefABCDEF".indexOf(eingabe) != -1)
-            ausgabe += "Hexdezimale Ziffer\n";
+            if("0123456789abcdefABCDEF".indexOf(eingabe) != -1)
+                ausgabe += "Hexdezimale Ziffer\n";
 
-        if(eingabe == '0' || eingabe == '1')
-            ausgabe += ("binäre Ziffer\n");
+            if(eingabe == '0' || eingabe == '1')
+                ausgabe += ("binäre Ziffer\n");
 
-        if("012345678".indexOf(eingabe) != -1)
-            ausgabe += ("oktale Ziffer\n");
+            if("012345678".indexOf(eingabe) != -1)
+                ausgabe += ("oktale Ziffer\n");
 
-        if(ausgabe.length() == 1)
-            System.out.println("\nUnbekannt");
-        else
-            System.out.print(ausgabe);
+            if(ausgabe.length() == 1)
+                System.out.println("\nUnbekannt");
+            else
+                System.out.print(ausgabe);
+        }
+        catch (Exception e) {} // idc
     }
 
     // Aufgabe 4
