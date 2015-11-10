1ff18d475f32b00066bbe494024f30a86befd675
Sebastian Kaim <sebb@sebb767.de>
2015-11-10 14:19:01 +0100

    Aufgabe 2

--- src/Uebung5.java
+++ src/Uebung5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by sebb on 11/10/15.
  */
@@ -12,5 +14,35 @@ public class Uebung5 {
             System.out.printf("%d°F\t%3.2f°C\n", i, (5./9) * (i-32));
     }
 
-    // 
+    // Aufgabe 2
+    public static void Querprodukt()
+    {
+        int in = -1,
+            querprodukt = 1;
+
+        do {
+            try
+            {
+                System.out.print("Geben Sie bitte eine Zahl ein: ");
+
+                // recreating clears the buffer :)
+                in = (new Scanner(System.in)).nextInt();
+
+                if(in < 0 || in > 1000000)
+                    throw new Exception();
+
+                break;
+            }
+            catch (Exception e)
+            {
+                System.out.println("FEHLER – Zahl ist ungueltig.");
+            }
+
+        } while (in < 0 || in > 1000000); // the clause is actually useless. But - while(true) is bad.
+
+        for(int rest = in; rest > 0; rest /= 10)
+            querprodukt *= rest%10;
+
+        System.out.printf("Das Querprodukt der Zahl %d betraegt %d.\n", in, querprodukt);
+    }
 }
