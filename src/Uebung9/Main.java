5d250746e0a48f502e95fa213e0e9db59046673e
Sebastian Kaim <sebb@sebb767.de>
2015-12-16 12:50:12 +0100

    Uebung 9

--- src/Uebung9/Main.java
+++ src/Uebung9/Main.java
@@ -1,5 +1,7 @@
 package Uebung9;
 
+import java.lang.reflect.Field;
+
 /**
  * Created by sebb on 12/9/15.
  */
@@ -13,12 +15,45 @@ public class Main {
                 new Auto("Ford", "Christenfisch", "Rot", 1, 1)
         };
 
+
+
+        /*
         int max = -1, idx = -1;
         for (int i = 0; i < autos.length; i++) if (autos[i].getHubraum() > max)
         {
             max = autos[i].getHubraum();
             idx = i;
         }
-        System.out.println(autos[idx]);
+        System.out.println(autos[idx]);*/
+        //scanClass((Object)autos);
+        scanClass(new Auto("VW", "Polo", "Weis", 85, 123),
+                new Auto("BMW", "M4", "Schwarz", 460, 321),
+                new Auto("Ford", "Christenfisch", "Rot", 1, 1));
+    }
+
+    public static void scanClass(Object... o)
+    {
+        for (Object instance : o) {
+            Field[] fields = instance.getClass().getDeclaredFields();
+
+            System.out.printf("Class: %s\n", instance.getClass().getName());
+            for (Field field : fields) {
+                field.setAccessible(true);
+
+                String mod = String.valueOf(field.getModifiers()), name = field.getName(), value;
+                try
+                {
+                    value = field.get(instance).toString();
+                }
+                catch (IllegalAccessException e)
+                {
+                   value = "Can't get value :/";
+                }
+
+                System.out.printf("[%s] %s = %s\n", mod, name, value);
+            }
+            System.out.println();
+        }
+
     }
 }
---
+++ src/Uebung9/Pizza.java
@@ -0,0 +1,13 @@
+package Uebung9;
+
+/**
+ * Created by sebb on 12/9/15.
+ */
+public class Pizza {
+    private Variation[] variations;
+
+    public Pizza(Variation... variationen)
+    {
+        variations = variationen;
+    }
+}
