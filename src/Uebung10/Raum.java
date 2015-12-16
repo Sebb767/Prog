0b53624ef6c92aa275430a73d5edf04bbf40e062
Sebastian Kaim <sebb@sebb767.de>
2015-12-16 12:58:09 +0100

    Uebung 10 fertig

--- src/Uebung10/Raum.java
+++ src/Uebung10/Raum.java
@@ -7,16 +7,39 @@ public class Raum {
     final int studenten;
     final String name;
 
+    private Student[] insassen;
+
     //private boolean belegt = false; // obsolet durch A3
     private Vorlesung v;
 
     public Raum(String name, int studenten) {
         this.studenten = studenten;
         this.name = name;
+        insassen = new Student[studenten];
     }
 
     public static void main(String[] args) {
+        // bla bla bla, kreiere Rau, bla bla bla
+    }
 
+    public boolean betrete(Student s)
+    {
+        for (int i = 0; i < insassen.length; i++) {
+            if(insassen[i] == null)
+            {
+                insassen[i] = s;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void verlasse(Student s)
+    {
+        for (int i = 0; i < insassen.length; i++) {
+            if(insassen[i].equals(s))
+                insassen[i] = null;
+        }
     }
 
     public boolean isBelegt() {
