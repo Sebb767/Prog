e997ca3c8558ae5d17502b4126c6be5c0ef4c4a5
Sebastian Kaim <sebb@sebb767.de>
2016-04-14 01:05:35 +0200

    Navicon implementiert

--- src/progII/exercise04/NaviCon.java
+++ src/progII/exercise04/NaviCon.java
@@ -5,13 +5,25 @@ package exercise04;
  */
 public class NaviCon implements NaviConInterface
 {
+    private GeoLocationManager mgr;
+
     @Override
     public void setGeoLocationManager(GeoLocationManager geoManager) {
-        
+        mgr = geoManager;
     }
 
     @Override
     public Point getCurrentPosition() throws InstantiationException, NullPointerException {
-        return null;
+        if(mgr == null)
+            throw new NullPointerException("NoGeoManager");
+
+        try
+        {
+            return mgr.currentLocation().moved(1, -1);
+        }
+        catch (IllegalAccessException e)
+        {
+            return this.getCurrentPosition();
+        }
     }
 }
