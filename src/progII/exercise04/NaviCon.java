72f15b270701672bbb63fd3e3f1ba24c3597ad1b
Sebastian Kaim <sebb@sebb767.de>
2016-04-14 00:59:39 +0200

    Skel

---
+++ src/progII/exercise04/NaviCon.java
@@ -0,0 +1,17 @@
+package exercise04;
+
+/**
+ * Created by sebb on 4/14/16.
+ */
+public class NaviCon implements NaviConInterface
+{
+    @Override
+    public void setGeoLocationManager(GeoLocationManager geoManager) {
+        
+    }
+
+    @Override
+    public Point getCurrentPosition() throws InstantiationException, NullPointerException {
+        return null;
+    }
+}
