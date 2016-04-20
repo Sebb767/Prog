21feeca1793114a2476a32a4100a795c33d7df85
Sebastian Kaim <sebb@sebb767.de>
2016-04-20 10:21:02 +0200

    Procedural excercise

---
+++ src/progII/cc/objectsanddatastructures/proc/shapes/Rectangle.java
@@ -0,0 +1,13 @@
+package cc.objectsanddatastructures.proc.shapes;
+
+/**
+ * Created by sebb on 4/20/16.
+ */
+public class Rectangle {
+    public double a,b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}
--- src/progII/cc/objectsanddatastructures/proc/shapes/ShapeCalculator.java
+++ src/progII/cc/objectsanddatastructures/proc/shapes/ShapeCalculator.java
@@ -1,27 +1,38 @@
 package cc.objectsanddatastructures.proc.shapes;
 
-public class ShapeCalculator
-{
-  public static double calculateArea(Object shape) throws NoSuchShapeException
-  {
-    if(shape instanceof Square)
-    {
-      Square s = (Square)shape;
-      return s.a * s.a;
+public class ShapeCalculator {
+    public static double calculateArea(Object shape) throws NoSuchShapeException {
+        if (shape instanceof Square) {
+            Square s = (Square) shape;
+            return s.a * s.a;
+        } else if (shape instanceof Circle) {
+            Circle c = (Circle) shape;
+            return c.r * c.r * Math.PI;
+        } else if (shape instanceof Square) {
+            Rectangle r = (Rectangle) shape;
+            return r.a * r.b;
+        }
+        throw new NoSuchShapeException("Given object is no known shape.");
     }
-    else if(shape instanceof Circle)
-    {
-      Circle c = (Circle)shape;
-      return c.r * c.r * Math.PI;
+
+    public static double calculatePerimeter(Object shape) throws NoSuchShapeException {
+        if (shape instanceof Square) {
+            Square s = (Square) shape;
+            return s.a * 4;
+        } else if (shape instanceof Circle) {
+            Circle c = (Circle) shape;
+            return 2 * c.r * Math.PI;
+        } else if (shape instanceof Square) {
+            Rectangle r = (Rectangle) shape;
+            return r.a * 2 + r.b * 2;
+        }
+        throw new NoSuchShapeException("Given object is no known shape.");
     }
-    throw new NoSuchShapeException("Given object is no known shape.");
-  }
-  
-  public static void main(String[] args)
-  {
-    Circle c = new Circle();
-    c.r = 5;
-    double area = calculateArea(c);
-    System.out.println(area);
-  }
+
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        c.r = 5;
+        double area = calculateArea(c);
+        System.out.println(area);
+    }
 }
