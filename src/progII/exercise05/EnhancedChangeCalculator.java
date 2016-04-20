78b46395d04dba480949640f0b932a8b54fc64c3
Sebastian Kaim <sebb@sebb767.de>
2016-04-20 10:59:57 +0200

    Formatierung

--- src/progII/exercise05/EnhancedChangeCalculator.java
+++ src/progII/exercise05/EnhancedChangeCalculator.java
@@ -6,62 +6,58 @@ package exercise05;
 // Spezialisierung soll aber darueber hinaus sicherstellen,
 // dass die Anzahl der ausgegeben Muenzen minimal ist.
 
-public class EnhancedChangeCalculator extends SimpleChangeCalculator 
-{
-	
-	// Hier fehlt Ihre Implementierung
+public class EnhancedChangeCalculator extends SimpleChangeCalculator {
+
+    // Hier fehlt Ihre Implementierung
 
 
-	@Override
-	public int[] getChange(int euros, int cent) {
-		while (cent > 100) {
-			euros ++;
-			cent -= 100;
-		}
+    @Override
+    public int[] getChange(int euros, int cent) {
+        while (cent > 100) {
+            euros++;
+            cent -= 100;
+        }
 
-		return new int[] {
-				euros / 2,
-				euros % 2,
-				cent / 50, // 50ct
-				(cent % 50) / 20, // 20ct
-				((cent % 50) % 20) / 10, // 10ct
-				(cent % 10) / 5, // 5ct
-				(cent % 5) / 2, // 2ct
-				cent % 2 // 1ct
-		};
-	}
+        return new int[]{
+                euros / 2,
+                euros % 2,
+                cent / 50, // 50ct
+                (cent % 50) / 20, // 20ct
+                ((cent % 50) % 20) / 10, // 10ct
+                (cent % 10) / 5, // 5ct
+                (cent % 5) / 2, // 2ct
+                cent % 2 // 1ct
+        };
+    }
 
-	// Die nachfolgende main-Methode kann genutzt werden, um das
-	// Ergebnis zu ueberpruefen.
-	public static void main(String[] args)
-	{
-		
-		int j = 0;
-		EnhancedChangeCalculator calc = new EnhancedChangeCalculator();
-		
-		System.out.println("\nZuerst ein simpler Fall: Es sollen 1 Euro und 70 Cent Wechselgeld zurückgegeben werden:");
-		int[] result = calc.getChange(1,70);
-		
-		System.out.println("\nDer Automat gibt folgende Muenzen zurueck:");
-		j = 0;
-		for (int i=0; i<Coin.availableCoins.length; i++)
-		{
-			System.out.println(result[i] + " x " + Coin.availableCoins[i].getName());
-			j += result[i];
-		}//endfor
-		System.out.println("Anzahl der Münzen: " + j);
-		
-		System.out.println("\nJetzt ein bißchen schwieriger: Es sollen 3 Euro und 88 Cent Wechselgeld zurückgegeben werden:");
-		result = calc.getChange(3,88);
-		
-		System.out.println("\nDer Automat gibt folgende Muenzen zurueck:");
-		j = 0;
-		for (int i=0; i<Coin.availableCoins.length; i++)
-		{
-			System.out.println(result[i] + " x " + Coin.availableCoins[i].getName());
-			j += result[i];
-		}//endfor
-		System.out.println("Anzahl der Münzen: " + j);
-	}//endmethod main
-	
+    // Die nachfolgende main-Methode kann genutzt werden, um das
+    // Ergebnis zu ueberpruefen.
+    public static void main(String[] args) {
+
+        int j = 0;
+        EnhancedChangeCalculator calc = new EnhancedChangeCalculator();
+
+        System.out.println("\nZuerst ein simpler Fall: Es sollen 1 Euro und 70 Cent Wechselgeld zurückgegeben werden:");
+        int[] result = calc.getChange(1, 70);
+
+        System.out.println("\nDer Automat gibt folgende Muenzen zurueck:");
+        j = 0;
+        for (int i = 0; i < Coin.availableCoins.length; i++) {
+            System.out.println(result[i] + " x " + Coin.availableCoins[i].getName());
+            j += result[i];
+        }//endfor
+        System.out.println("Anzahl der Münzen: " + j);
+
+        System.out.println("\nJetzt ein bißchen schwieriger: Es sollen 3 Euro und 88 Cent Wechselgeld zurückgegeben werden:");
+        result = calc.getChange(3, 88);
+
+        System.out.println("\nDer Automat gibt folgende Muenzen zurueck:");
+        j = 0;
+        for (int i = 0; i < Coin.availableCoins.length; i++) {
+            System.out.println(result[i] + " x " + Coin.availableCoins[i].getName());
+            j += result[i];
+        }//endfor
+        System.out.println("Anzahl der Münzen: " + j);
+    }//endmethod main
+
 }//endclass EnhancedChangeCalculator
