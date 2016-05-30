3c79fac8ac52953e2bd11638a1015b83451e2b70
Sebb767 <sebb@sebb767.de>
2016-05-30 13:38:23 +0200

    Übung 8 skel

---
+++ src/progII/exercise08/FileDecryptor.java
@@ -0,0 +1,27 @@
+package exercise08;
+
+import java.io.*;
+
+// Mit Hilfe dieser Klassen sollen verschluesselte Dateien
+// auf der Konsole ausgegeben werden. Dazu ist die Datei zu
+// oeffnen, mit einem Decryptor-Decorator zu versehen und
+// einzulesen. Jedes eingelesene und entschluesselte Zeichen
+// soll unmittelbar auf der Konsole ausgegeben werden. 
+
+public class FileDecryptor {
+
+	// Diese Methode muss nach den Vorgaben implementiert werden.
+	public static void decryptFile(String filename)
+	{
+		// Hier fehlt die Implementierung
+	}
+	
+	// Diese main-Methode zeigt die Verwednung der zu 
+	// implementierenden Methode. An der Ausgabe koennen Sie 
+	// erkennen, ob Ihre Implementierung funktioniert.
+	public static void main(String[] args)
+	{
+		decryptFile("src/exercis08/data.crypt");
+	}
+	
+}
