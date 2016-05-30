974a1b4ef8ac0c095ecfc316e8c57dd3bcfbc367
Sebb767 <sebb@sebb767.de>
2016-05-30 14:36:50 +0200

    Funktionierende (?) Lösung

---
+++ src/progII/exercise08/DecryptInputStream.java
@@ -0,0 +1,29 @@
+package exercise08;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by intellij on 5/30/16.
+ */
+public class DecryptInputStream extends InputStream {
+    private InputStream is;
+    private /* super secret */ int key;
+
+    public DecryptInputStream(InputStream is, int key) {
+        this.is = is;
+        this.key = key;
+    }
+
+    @Override
+    public int read() throws IOException {
+        int res = is.read();
+        return res < 0 ? res : res ^ key;
+    }
+
+    @Override
+    public void close() throws IOException {
+        is.close();
+        super.close();
+    }
+}
---
+++ src/progII/exercise08/EncryptOutputStream.java
@@ -0,0 +1,28 @@
+package exercise08;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by intellij on 5/30/16.
+ */
+public class EncryptOutputStream extends OutputStream {
+    private OutputStream os;
+    private /* super secret */ int key;
+
+    public EncryptOutputStream(OutputStream os, int key) {
+        this.os = os;
+        this.key = key;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        os.write(b ^ key);
+    }
+
+    @Override
+    public void close() throws IOException {
+        os.close();
+        super.close();
+    }
+}
