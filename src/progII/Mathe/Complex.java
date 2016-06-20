726e6b018d3487d4af0466be6ef99c184574f5aa
Sebb767 <sebb@sebb767.de>
2016-06-20 15:16:02 +0200

    U11 skel

--- src/progII/Mathe/Complex.java
+++ src/progII/Mathe/Complex.java
@@ -4,4 +4,22 @@ package Mathe;
  * Created by sebb on 5/7/16.
  */
 public class Complex {
+    double real, imag;
+
+    public Complex(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    public Complex(double real) {
+        this.real = real;
+    }
+
+    public double real() {
+        return real;
+    }
+
+    public double imag() {
+        return imag;
+    }
 }
