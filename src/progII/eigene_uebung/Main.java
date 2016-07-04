332fdf0a4911e73b88e754482df8296a92f5a32b
Sebb767 <sebb@sebb767.de>
2016-07-04 15:19:37 +0200

    tests

--- src/progII/eigene_uebung/Main.java
+++ src/progII/eigene_uebung/Main.java
@@ -3,9 +3,15 @@ package eigene_uebung;
 public class Main {
 
     public static void main(String[] args) {
+        FileIterator fi = new FileIterator("/home/proj/Projekte/fh/Prog/src/progII/eigene_uebung/logfile.txt");
+        LogStore ls = new LogStore();
 
+        while (fi.hasNext())
+            ls.add(new LogEntry(fi.next()));
 
-
+        LogEntry[] output = ls.logsAsArray();
+        for (LogEntry e: output)
+            System.out.println(e);
     }
 
 }
