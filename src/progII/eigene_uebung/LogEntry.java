1aecffb6b7a1a90475b8359fa5a2b85153cab528
Sebb767 <sebb@sebb767.de>
2016-07-04 14:19:10 +0200

    LogEntry Finished

--- src/progII/eigene_uebung/LogEntry.java
+++ src/progII/eigene_uebung/LogEntry.java
@@ -1,5 +1,27 @@
 package eigene_uebung;
 
 public class LogEntry {
+    protected final long timestamp;
+    protected final String name, failureDescription;
+    protected final int id;
 
+    protected static int ID = 0;
+
+    public LogEntry(final String line) {
+
+        final String[] entry = line.split("-", 3);
+        this.timestamp = Long.parseLong(entry[0]);
+        this.name = entry[1];
+        this.failureDescription = entry[2];
+        id = ID++;
+    }
+
+    /**
+     * Nur Setter, da hier nichts verändert werden muss.
+     */
+
+    @Override
+    public String toString() {
+        return timestamp + "-" + name + "-" + failureDescription;
+    }
 }
