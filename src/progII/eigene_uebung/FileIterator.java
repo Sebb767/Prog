a80aa8e14e5bb65a37b7faa4dbd495a550682821
Sebb767 <sebb@sebb767.de>
2016-07-04 14:07:57 +0200

    File Iterator Finished

--- src/progII/eigene_uebung/FileIterator.java
+++ src/progII/eigene_uebung/FileIterator.java
@@ -1,7 +1,11 @@
 package eigene_uebung;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
-
+import java.util.NoSuchElementException;
 
 
 /**
@@ -17,19 +21,45 @@ import java.util.Iterator;
 
 
 public class FileIterator implements Iterator<String>{
+    protected String filename, line;
+    protected BufferedReader br;
+    boolean read = false;
 
     public FileIterator(String filename) {
+        try
+        {
+            this.filename = filename;
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+        }
+        catch (IOException ex)
+        {
 
+        }
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        try
+        {
+            if(!read)
+            {
+                line = br.readLine();
+                read = true;
+            }
+
+            return line != null;
+        }
+        catch (IOException e) { read = true; line = null; return false; }
     }
 
     @Override
-    public String next() {
-        return null;
+    public String next()
+    {
+        if(!this.hasNext())
+            throw new NoSuchElementException();
+
+        read = false;
+        return line;
     }
 
     @Override
