7b71b80b3301727ba65d4a3282e22182f2f5fa05
Sebb767 <sebb@sebb767.de>
2016-07-04 14:35:43 +0200

    Mostly done

--- src/progII/eigene_uebung/LogStore.java
+++ src/progII/eigene_uebung/LogStore.java
@@ -1,21 +1,34 @@
 package eigene_uebung;
 
+import sun.rmi.runtime.Log;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 
 
 public class LogStore {
+    Collection<LogEntry> c;
 
     public LogStore() {
+        c = new ArrayList<>();
+    }
 
+    public void add(LogEntry e) {
+        c.add(e);
     }
 
     public LogEntry[] logsAsArray() {
-        return null;
+        return c.toArray(new LogEntry[0]);
     }
 
     public LogEntry[] logsBetween(Date from, Date to) {
-        return null;
+
+        return (LogEntry[]) c
+                .stream()
+                .map(x -> from.getTime() >= x.getTimestamp() && to.getTime() <= x.getTimestamp())
+                .toArray();
     }
 
     public LogEntry[] logsForSysten(String systemName) {
