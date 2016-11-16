bbd1a2cb3447291924eb0647c21f29457e0b9dda
SimonOne3 <simonneu@outlook.de>
2016-11-16 16:22:22 +0100

    Add files via upload

---
+++ src/tutorium/aufgabe2_smartphone/Smartphoneverwaltung.java
@@ -0,0 +1,17 @@
+package aufgabe2_smartphone;
+
+import java.util.ArrayList;
+
+public class Smartphoneverwaltung {
+
+	private ArrayList<Smartphone> list = new ArrayList<>();
+
+	public void addSmartphone(Smartphone s) {
+		list.add(s);
+	}
+
+	public Smartphone[] getAllSmartphone() {
+		return list.toArray(new Smartphone[0]);
+	}
+
+}
