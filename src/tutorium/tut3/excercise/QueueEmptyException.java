f269ea317cc98c84b407d4e457ed1c67737cf2e5
Sebb767 <sebb@sebb767.de>
2016-11-23 13:03:03 +0100

    Added Queue

---
+++ src/tutorium/tut3/excercise/QueueEmptyException.java
@@ -0,0 +1,25 @@
+package tut3.excercise;
+
+/**
+ * Created by proj on 11/17/16.
+ */
+public class QueueEmptyException extends Exception {
+    public QueueEmptyException() {
+    }
+
+    public QueueEmptyException(String message) {
+        super(message);
+    }
+
+    public QueueEmptyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public QueueEmptyException(Throwable cause) {
+        super(cause);
+    }
+
+    public QueueEmptyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
