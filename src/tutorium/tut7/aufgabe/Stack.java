d7070b51658d5d32733e5db11cbe890a41ee3fd3
Sebb767 <sebb@sebb767.de>
2016-12-28 17:08:56 +0100

    Tut 7 A1

---
+++ src/tutorium/tut7/aufgabe/Stack.java
@@ -0,0 +1,42 @@
+package tut7.aufgabe;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by proj on 12/28/16.
+ */
+public class Stack<T extends Stackable> implements Iterable<T> {
+
+    protected ArrayList<T> content = new ArrayList<>();
+
+    public void push(T in)
+    {
+        content.add(in);
+    }
+
+    public T pop()
+    {
+        return content.remove(content.size() - 1);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            ArrayList<T> c = content;
+
+            @Override
+            public boolean hasNext() {
+                return c.size() > 0;
+            }
+
+            @Override
+            public T next() {
+                if(!hasNext())
+                    throw new NoSuchElementException();
+                return c.get(c.size() - 1);
+            }
+        };
+    }
+}
---
+++ src/tutorium/tut7/aufgabe/Stackable.java
@@ -0,0 +1,7 @@
+package tut7.aufgabe;
+
+/**
+ * Created by proj on 12/28/16.
+ */
+public interface Stackable {
+}
