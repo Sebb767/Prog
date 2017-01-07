32dfe221fd9212c6462f6d54a1ac85243e97c0c8
Sebb767 <sebb@sebb767.de>
2017-01-08 00:12:53 +0100

    Aufg. 4 fast fertig

---
+++ src/tutorium/tut7/aufgabe/ParallelProcesserTest.java
@@ -0,0 +1,30 @@
+package tut7.aufgabe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by proj on 1/8/17.
+ */
+public class ParallelProcesserTest {
+    @Test
+    public void TestRun()
+    {
+        Integer[] data = new Integer[250000], nd;
+        for (int i = 0; i < 250000; i++)
+            data[i] = i;
+
+        ParallelProcessor<Integer> pp = new ParallelProcessor<Integer>(data, 4, false, new Processor<Integer>() {
+            @Override
+            public Integer process(Integer input) {
+                return 1;
+            }
+        });
+
+        pp.processInput();
+
+        nd = pp.getInput();
+        for(Integer i : nd)
+            assertEquals(i, (Integer)1);
+    }
+}
---
+++ src/tutorium/tut7/aufgabe/Processor.java
@@ -0,0 +1,8 @@
+package tut7.aufgabe;
+
+/**
+ * Created by proj on 1/7/17.
+ */
+public interface Processor<T> {
+    T process(T input);
+}
