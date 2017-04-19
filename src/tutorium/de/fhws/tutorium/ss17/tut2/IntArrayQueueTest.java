0810bde4e1d039e85f8ffaba1af2dae19ea17080
Sebb767 <sebb@sebb767.de>
2017-04-19 14:58:52 +0200

    Added ArrayQueue test

---
+++ src/tutorium/de/fhws/tutorium/ss17/tut2/IntArrayQueueTest.java
@@ -0,0 +1,51 @@
+package de.fhws.tutorium.ss17.tut2;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IntArrayQueueTest {
+    protected IntArrayQueue iaq;
+
+    @Before
+    public void prepareQueue()
+    {
+        iaq = new IntArrayQueue(200);
+    }
+
+    @Test
+    public void testIfNewQueueIsEmpty()
+    {
+        assertTrue(iaq.empty());
+    }
+
+    @Test
+    public void testOrderOfQueue() throws QueueEmptyException
+    {
+        iaq.enqueue(1);
+        iaq.enqueue(2);
+
+        assertFalse(iaq.empty());
+        assertEquals(1, iaq.dequeue());
+
+        assertFalse(iaq.empty());
+        assertEquals(2, iaq.dequeue());
+
+        assertTrue(iaq.empty());
+    }
+
+    @Test
+    public void testQueueException()
+    {
+        assertTrue(iaq.empty());
+
+        try {
+            iaq.dequeue();
+            assertTrue(false);
+        }
+        catch (QueueEmptyException ex)
+        {
+            assertTrue(true);
+        }
+    }
+}
